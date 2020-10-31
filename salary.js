32776792e3436ed12b1fe65482eb419e7a914881
Oluwakemi Oyindamola Akindele <brandykemi@gmail.com>
2020-10-31 16:56:50 -0700

    New Feat

---
+++ salary.js
@@ -0,0 +1,22 @@
+
+    const normalPay = 1000;
+
+    const extraPay = 200; 
+    
+    standardHour = 4;
+    
+    const Salary = function(){
+      const hoursWorkedPerDay = document.querySelector('#normalHour').value;
+        if(hoursWorkedPerDay > 4 ){
+
+          document.querySelector('#display').textContent = (normalPay * standardHour) + ((hoursWorkedPerDay - standardHour) * extraPay);
+        
+        }else{
+          document.querySelector('#display').textContent = hoursWorkedPerDay * normalPay;
+        }
+      }
+      const result = document.querySelector(".answer");
+      result.onclick = function() {
+          Salary();
+        
+};
